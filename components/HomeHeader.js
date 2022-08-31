92fd2f2969262145f35c67ba85ac3a70038a732c
kellonparkinson <kellon.parkinson@gmail.com>
2022-08-31 17:12:49 -0600

    created details comps and added them to index.js

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -3,7 +3,7 @@ import { View, Text, Image, TextInput } from 'react-native'
 import { COLORS, FONTS, SIZES, assets } from '../constants'
 import Kellon from '../assets/images/person05.png'
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
   return (
     <View style={{
       backgroundColor: COLORS.primary,
@@ -61,7 +61,7 @@ const HomeHeader = () => {
           <TextInput 
             placeholder='Search NFTs'
             style={{ flex: 1 }}
-            onChangeText={() => {}}
+            onChangeText={onSearch}
           />
         </View>
       </View>
